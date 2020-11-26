2624caf6660cf744438a0058f50ced6517a8cae2
Maksim Karalevich <m@karalevich.com>
2020-11-25 16:42:01 -0800

    add errors for register resolver

---
+++ readme.md
@@ -0,0 +1,9 @@
+### LilReddit
+
+Postgres, Mikro-ORM, Express, Apollo Server, TypeGraphQL
+
+```bash
+$ yarn install
+$ yarn watch
+$ yarn dev
+```
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,8 +1,17 @@
 import { User } from "./../entities/User";
 import { MyContext } from "./../../types";
-import { Arg, Ctx, Field, InputType, Resolver, Mutation } from "type-graphql";
+import {
+	Arg,
+	Ctx,
+	Field,
+	InputType,
+	Resolver,
+	Mutation,
+	ObjectType,
+} from "type-graphql";
 import argon2 from "argon2";
 
+// InputType is used for arguments
 @InputType()
 class UsernamePasswordInput {
 	@Field()
@@ -11,19 +20,107 @@ class UsernamePasswordInput {
 	password: string;
 }
 
+@ObjectType()
+class FieldError {
+	@Field()
+	field: string;
+	@Field()
+	message: string;
+}
+
+// ObjectType is returned from mutations
+@ObjectType()
+class UserResponse {
+	@Field(() => [FieldError], { nullable: true })
+	errors?: FieldError[];
+	@Field(() => User, { nullable: true })
+	user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-	@Mutation(() => User)
+	// Register user
+	@Mutation(() => UserResponse)
 	async register(
 		@Arg("options") options: UsernamePasswordInput,
 		@Ctx() { em }: MyContext
-	) {
+	): Promise<UserResponse> {
+		if (options.username.length <= 2) {
+			return {
+				errors: [
+					{
+						field: "username",
+						message: "too short username",
+					},
+				],
+			};
+		}
+
+		if (options.password.length <= 2) {
+			return {
+				errors: [
+					{
+						field: "password",
+						message: "too short password",
+					},
+				],
+			};
+		}
+
 		const hashedPassword = await argon2.hash(options.password);
 		const user = em.create(User, {
 			username: options.username,
 			password: hashedPassword,
 		});
-		await em.persistAndFlush(user);
-		return user;
+		try {
+			await em.persistAndFlush(user);
+		} catch (err) {
+			// duplicate user err
+			if (err.code === "23505") {
+				return {
+					errors: [
+						{
+							field: "username",
+							message: "user alread exists",
+						},
+					],
+				};
+			}
+			console.log("message: ", err.message);
+		}
+		return { user };
+	}
+
+	// Login user
+	@Mutation(() => UserResponse)
+	async login(
+		@Arg("options") options: UsernamePasswordInput,
+		@Ctx() { em }: MyContext
+	): Promise<UserResponse> {
+		const user = await em.findOne(User, {
+			username: options.username.toLowerCase(),
+		});
+		if (!user) {
+			return {
+				errors: [
+					{
+						field: "username",
+						message: "user doesn't exist",
+					},
+				],
+			};
+		}
+		const valid = await argon2.verify(user.password, options.password);
+		if (!valid) {
+			return {
+				errors: [
+					{
+						field: "password",
+						message: "password doesn't match",
+					},
+				],
+			};
+		}
+		return { user };
 	}
 }
