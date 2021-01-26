49bce738cdb14657054541decd4fdc140fe423a2
Maksim Karalevich <m@karalevich.com>
2021-01-25 23:16:08 -0800

    add me

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -8,6 +8,7 @@ import {
 	Resolver,
 	Mutation,
 	ObjectType,
+	Query,
 } from "type-graphql";
 import argon2 from "argon2";
 
@@ -47,11 +48,22 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+	@Query(() => User, { nullable: true })
+	async me(@Ctx() { req, em }: MyContext) {
+		// you are not logged in
+		if (!req.session.userId) {
+			return null;
+		}
+
+		const user = await em.findOne(User, { id: req.session.userId });
+		return user;
+	}
+
 	// Register user
 	@Mutation(() => UserResponse)
 	async register(
 		@Arg("options") options: UsernamePasswordInput,
-		@Ctx() { em }: MyContext
+		@Ctx() { req, em }: MyContext
 	): Promise<UserResponse> {
 		if (options.username.length <= 2) {
 			return {
@@ -130,7 +142,9 @@ export class UserResolver {
 			};
 		}
 
-		req.session!.userId = user.id;
+		// store user id session
+		// this will set a cookie on the user
+		req.session.userId = user.id;
 		return { user };
 	}
 }
